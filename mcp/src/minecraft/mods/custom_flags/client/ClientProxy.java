0f84d648654f07b134681469e8ebbcd60c9d8f20
amedw <mr.amedw@gmail.com>
2013-08-02 23:07:22 +1000

    Auto Commit Fri 02/08/2013-23:07:21.99

--- mcp/src/minecraft/mods/custom_flags/client/ClientProxy.java
+++ mcp/src/minecraft/mods/custom_flags/client/ClientProxy.java
@@ -3,8 +3,12 @@ package mods.custom_flags.client;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import mods.custom_flags.CommonProxy;
+import mods.custom_flags.CustomFlags;
 import mods.custom_flags.blocks.TileEntityFlagPole;
+import mods.custom_flags.client.renderer.FlagPoleItemRenderer;
 import mods.custom_flags.client.renderer.FlagPoleTileRenderer;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
 
 /**
  * User: nerd-boy
@@ -22,5 +26,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers() {
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityFlagPole.class, new FlagPoleTileRenderer());
+
+        MinecraftForgeClient.registerItemRenderer(CustomFlags.blockFlagPole.blockID, new FlagPoleItemRenderer());
     }
 }
--- mcp/src/minecraft/mods/custom_flags/client/renderer/FlagPoleItemRenderer.java
+++ mcp/src/minecraft/mods/custom_flags/client/renderer/FlagPoleItemRenderer.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import net.minecraftforge.client.IItemRenderer;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Created by Aaron on 2/08/13.
@@ -24,31 +25,115 @@ public class FlagPoleItemRenderer implements IItemRenderer {
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
         Icon icon = Block.wood.getIcon(2,0);
-
+        Tessellator tess = Tessellator.instance;
+        float size = 1.5F;
         switch (type) {
             case ENTITY:
-                break;
+                GL11.glTranslatef(1F, 0, 1F);
+                size = 1;
             case EQUIPPED:
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(7F/16F, size, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(9F/16F, size, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(9F/16F, 0, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(7F/16F, 0, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(0));
+                tess.draw();
+
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(7F/16F, 0, 0F/16F, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(7F/16F, 0, 2F/16F, icon.getInterpolatedU(8), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(7F/16F, size, 2F/16F, icon.getInterpolatedU(8), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(7F/16F, size, 0F/16F, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
+                tess.draw();
+
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(7F/16F, 0, 2F/16F, icon.getInterpolatedU(8), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(9F/16F, 0, 2F/16F, icon.getInterpolatedU(12), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(9F/16F, size, 2F/16F, icon.getInterpolatedU(12), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(7F/16F, size, 2F/16F, icon.getInterpolatedU(8), icon.getInterpolatedV(16));
+                tess.draw();
+
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(9F/16F, size, 0F/16F, icon.getInterpolatedU(12), icon.getInterpolatedV(16));
+
+                tess.addVertexWithUV(9F/16F, size, 2F/16F, icon.getInterpolatedU(16), icon.getInterpolatedV(16));
+
+                tess.addVertexWithUV(9F/16F, 0, 2F/16F, icon.getInterpolatedU(16), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(9F/16F, 0, 0F/16F, icon.getInterpolatedU(12), icon.getInterpolatedV(0));
+                tess.draw();
+
+
+                icon = Block.wood.getIcon(0,0);
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(7F / 16F, 0, 0F / 16F, icon.getInterpolatedU(6), icon.getInterpolatedV(6));
+                tess.addVertexWithUV(9F / 16F, 0, 0F / 16F, icon.getInterpolatedU(10), icon.getInterpolatedV(6));
+                tess.addVertexWithUV(9F / 16F, 0, 2F / 16F, icon.getInterpolatedU(10), icon.getInterpolatedV(10));
+                tess.addVertexWithUV(7F / 16F, 0, 2F / 16F, icon.getInterpolatedU(6), icon.getInterpolatedV(10));
+                tess.draw();
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(9F / 16F, size, 0F / 16F, icon.getInterpolatedU(6), icon.getInterpolatedV(6));
+                tess.addVertexWithUV(7F / 16F, size, 0F / 16F, icon.getInterpolatedU(10), icon.getInterpolatedV(6));
+                tess.addVertexWithUV(7F / 16F, size, 2F / 16F, icon.getInterpolatedU(10), icon.getInterpolatedV(10));
+                tess.addVertexWithUV(9F / 16F, size, 2F / 16F, icon.getInterpolatedU(6), icon.getInterpolatedV(10));
+                tess.draw();
+
+
                 break;
             case EQUIPPED_FIRST_PERSON:
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(7F/16F, 1, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(9F/16F, 1, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(9F/16F, 0, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(7F/16F, 0, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(0));
+                tess.draw();
+
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(7F/16F, 0, 0F/16F, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(7F/16F, 0, 2F/16F, icon.getInterpolatedU(8), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(7F/16F, 1, 2F/16F, icon.getInterpolatedU(8), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(7F/16F, 1, 0F/16F, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
+                tess.draw();
+
+
+
                 break;
             case INVENTORY:
 
-                Tessellator tess = Tessellator.instance;
+
 
                 tess.startDrawingQuads();
-                tess.addVertexWithUV(7, 0, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(0));
-                tess.addVertexWithUV(8, 1, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
-                tess.addVertexWithUV(8, 12, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
-                tess.addVertexWithUV(7, 11, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(6, 13, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(8, 15, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(8, 5, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(6, 3, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(0));
                 tess.draw();
 
+
                 tess.startDrawingQuads();
-                tess.addVertexWithUV(9, 0, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(0));
-                tess.addVertexWithUV(8, 1, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
-                tess.addVertexWithUV(8, 12, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
-                tess.addVertexWithUV(9, 11, 0, icon.getInterpolatedU(0), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(10, 3, 0, icon.getInterpolatedU(8), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(8, 5, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(0));
+                tess.addVertexWithUV(8, 15, 0, icon.getInterpolatedU(4), icon.getInterpolatedV(16));
+                tess.addVertexWithUV(10, 13, 0, icon.getInterpolatedU(8), icon.getInterpolatedV(16));
                 tess.draw();
+
+
+                icon = Block.wood.getIcon(0,0);
+
+                tess.startDrawingQuads();
+                tess.addVertexWithUV(8, 2, 0, icon.getInterpolatedU(6), icon.getInterpolatedV(10));
+                tess.addVertexWithUV(6, 3, 0, icon.getInterpolatedU(10), icon.getInterpolatedV(10));
+                tess.addVertexWithUV(8, 5, 0, icon.getInterpolatedU(10), icon.getInterpolatedV(6));
+                tess.addVertexWithUV(10, 3, 0, icon.getInterpolatedU(6), icon.getInterpolatedV(6));
+                tess.draw();
+
 
                 break;
             case FIRST_PERSON_MAP:
