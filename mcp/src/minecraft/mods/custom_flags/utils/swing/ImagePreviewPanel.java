482dbe354442769b98a33e0dd2e5eed12afd9ef7
amedw <mr.amedw@gmail.com>
2013-08-03 20:15:00 +1000

    Custom Flags Auto Commit

--- mcp/src/minecraft/mods/custom_flags/utils/swing/ImagePreviewPanel.java
+++ mcp/src/minecraft/mods/custom_flags/utils/swing/ImagePreviewPanel.java
@@ -1,8 +1,10 @@
 package mods.custom_flags.utils.swing;
 
+
 import javax.swing.*;
 import java.awt.*;
-import java.beans.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 
 public class ImagePreviewPanel extends JPanel
@@ -22,7 +24,6 @@ public class ImagePreviewPanel extends JPanel
     public void propertyChange(PropertyChangeEvent e) {
         String propertyName = e.getPropertyName();
 
-        // Make sure we are responding to the right event.
         if (propertyName.equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)) {
             File selection = (File)e.getNewValue();
             String name;
@@ -32,10 +33,6 @@ public class ImagePreviewPanel extends JPanel
             else
                 name = selection.getAbsolutePath();
 
-            /*
-             * Make reasonably sure we have an image format that AWT can
-             * handle so we don't try to draw something silly.
-             */
             if ((name != null) &&
                     name.toLowerCase().endsWith(".jpg") ||
                     name.toLowerCase().endsWith(".jpeg") ||
@@ -54,11 +51,6 @@ public class ImagePreviewPanel extends JPanel
         height = image.getHeight(this);
         double ratio = 1.0;
 
-        /*
-         * Determine how to scale the image. Since the accessory can expand
-         * vertically make sure we don't go larger than 150 when scaling
-         * vertically.
-         */
         if (width >= height) {
             ratio = (double)(ACCSIZE-5) / width;
             width = ACCSIZE-5;
@@ -83,14 +75,6 @@ public class ImagePreviewPanel extends JPanel
     public void paintComponent(Graphics g) {
         g.setColor(bg);
 
-        /*
-         * If we don't do this, we will end up with garbage from previous
-         * images if they have larger sizes than the one we are currently
-         * drawing. Also, it seems that the file list can paint outside
-         * of its rectangle, and will cause odd behavior if we don't clear
-         * or fill the rectangle for the accessory before drawing. This might
-         * be a bug in JFileChooser.
-         */
         g.fillRect(0, 0, ACCSIZE, getHeight());
         g.drawImage(image, getWidth() / 2 - width / 2 + 5,
                 getHeight() / 2 - height / 2, this);
