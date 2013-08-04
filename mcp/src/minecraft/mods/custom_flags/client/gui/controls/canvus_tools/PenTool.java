7339e6d7d3185d97fd04340966e613cec300e7f7
amedw <mr.amedw@gmail.com>
2013-08-04 16:45:29 +1000

    Custom Flags Auto Commit

--- mcp/src/minecraft/mods/custom_flags/client/gui/controls/canvus_tools/PenTool.java
+++ mcp/src/minecraft/mods/custom_flags/client/gui/controls/canvus_tools/PenTool.java
@@ -20,8 +20,12 @@ public class PenTool implements ITool {
             pixelsOverlay[i] = pixelsCurrent[i];
         }
 
-        if (x > -1 &&  x < ImageData.IMAGE_RES && y > -1 && y < ImageData.IMAGE_RES){
-            pixelsOverlay[x+ImageData.IMAGE_RES*y] = rgb;
+        if(shift){
+            drawLine(x, last_x, y, last_y, pixelsOverlay, rgb);
+        }else{
+            if (x > -1 &&  x < ImageData.IMAGE_RES && y > -1 && y < ImageData.IMAGE_RES){
+                pixelsOverlay[x+ImageData.IMAGE_RES*y] = rgb;
+            }
         }
 
         overlay.func_110564_a();
@@ -33,7 +37,7 @@ public class PenTool implements ITool {
 
         if(shift){
 
-            
+            drawLine(x, last_x, y, last_y, pixelsCurrent, rgb);
 
         }else{
             if (x > -1 &&  x < ImageData.IMAGE_RES && y > -1 && y < ImageData.IMAGE_RES){
@@ -43,6 +47,52 @@ public class PenTool implements ITool {
 
         last_x = x;
         last_y = y;
+    }
+
+    private void drawLine(int x0, int x1, int y0, int y1, int[] pixelsCurrent, int rgb) {
+
+        boolean steep = Math.abs(y1 - y0) > Math.abs(x1- x0);
+        if(steep){
+            int temp = x0;
+            x0 = x1;
+            x1 = temp;
+
+            temp = y0;
+            y0 = y1;
+            y1 = temp;
+        }
+        if(x0 > x1){
+            int temp = x0;
+            x0 = x1;
+            x1 = temp;
+
+            temp = y0;
+            y0 = y1;
+            y1 = temp;
+        }
+
+        int deltaX = x1 - x0;
+        int deltaY = Math.abs(y1 - y0);
+
+        int error = deltaX / 2;
+        int yStep = y0<y1?1:-1;
+        int y = y0;
+        for(int x = x0; x < x1; x++){
+            if (x > -1 &&  x < ImageData.IMAGE_RES && y > -1 && y < ImageData.IMAGE_RES){
+                if(steep){
+                    pixelsCurrent[y+ImageData.IMAGE_RES*x] = rgb;
+                }else{
+                    pixelsCurrent[x+ImageData.IMAGE_RES*y] = rgb;
+                }
+            }
+
+            error = error - deltaY;
+            if(error < 0){
+                y = y + yStep;
+                error = error - deltaX;
+            }
+        }
+
     }
 
 
